eda5e7fc3b8aa1b53768e0bcce5d40dd830e03d0
Jiachen Wang <21996634+wjcGreenPig@users.noreply.github.com>
2018-04-14 16:27:27 -0500

    Update apps.py

--- website/codesearch/apps.py
+++ website/codesearch/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
 
-
 class CodesearchConfig(AppConfig):
     name = 'codesearch'
+    def ready(self):
+        # Singleton utility
+        # We load them here to avoid multiple instantiation across other
+        # modules, that would take too much time.
+        
+        pass
+     
+
